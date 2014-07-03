bb9ba4bafca2c99af093b441e7603c6471899099
shogun656 <whaddad16@yahoo.com>
2014-07-03 17:29:17 -0400

    this is what ive got so far

--- Crash/src/com/wes/crash/MainActivity.java
+++ Crash/src/com/wes/crash/MainActivity.java
@@ -1,14 +1,17 @@
 package com.wes.crash;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 import com.kc.main.MainView;
 
 public class MainActivity extends Activity {
-	@Override
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(new MainView(this));
 	}
+	
 }
