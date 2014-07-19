c33afa56758949bee79036750cd37e1e7496dd10
Wesam <whaddad16@yahoo.com>
2014-07-19 12:12:36 -0400

    what i got so far

--- Crash/src/com/kc/main/MainView.java
+++ Crash/src/com/kc/main/MainView.java
@@ -6,7 +6,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 
 import com.example.crash.R;
 import com.kc.inter.Drawable;
@@ -23,37 +28,37 @@ public class MainView extends View implements Updatable, Drawable{
 	ArrayList<BaseGameObject> BL;
 	Pacer Pb,pc;
 	Coin Co;
+	float x, y;
+	boolean ispress;
 	public MainView(Context C) {
 		super(C);
 		BM = new Benchmark();
 		WT = new WinTool(C);
 		Coin_Image = BitmapFactory.decodeResource(this.getResources(), R.drawable.coin);
 		Co = new Coin(Coin_Image,300f,300f,171f,166f,-1.5f,.1f);
-		/*
 		Dirt_Image = BitmapFactory.decodeResource(this.getResources(), R.drawable.tiledirt);
 		Bubble_Image = BitmapFactory.decodeResource(this.getResources(), R.drawable.bubble);
+		
 		BL = new ArrayList<BaseGameObject>();
 		Pb = new Pacer(Bubble_Image,BL,1000,154f,148f);
 		Pb.setMinspeed(.8f);
 		Pb.setLimit(10);
 		Pb.getT().setThreshold(1000);
-		pc = new Pacer(Coin_Image,BL,5000,171f,166f);
-		*/
 	}
 	public void Update(long mi) {
-		/*
 		Pb.Update(mi);
-		pc.Update(mi);
 		for (int i = BL.size()-1; i > -1; i--) {
 			BaseGameObject BG = BL.get(i);
 			BG.Update(mi);
 			if (BG.getY() > WT.getScreenHeight()) BL.remove(BG);
 		}
-		*/
+		if(ispress) {
+			//check collision
+			boolean b = Co.getCoords().contains((int)x,(int)y);
+		}
 		Co.update(mi);
 	}
 	public void Draw(Canvas C) {
-		/*
 		for (BaseGameObject BG : BL) BG.Draw(C);
 		Paint P = new Paint();
 		P.setTextSize(100f);
@@ -61,7 +66,6 @@ public class MainView extends View implements Updatable, Drawable{
 		//P.setStrokeWidth(10f);
 		P.setARGB(255,0,0,0);
 		C.drawText(""+BL.size(),100,100,P);
-		*/
 		Co.Draw(C);
 	}
 	public void onDraw(Canvas C) {
@@ -71,4 +75,23 @@ public class MainView extends View implements Updatable, Drawable{
 		Update(BM.getMillisecondsElapsed());
 		Draw(C);
 	}
+	public boolean onTouchEvent(MotionEvent event) {
+		try{
+			Thread.sleep(33);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		x = event.getX();
+		y = event.getY();
+		if (MotionEvent.ACTION_DOWN == event.getAction()) {
+			ispress = true;
+		}
+		if (MotionEvent.ACTION_UP == event.getAction()) {
+			ispress = false;
+		}
+		
+		return true;
+	}
+	
 }
--- Crash/src/com/wes/crash/MainActivity.java
+++ Crash/src/com/wes/crash/MainActivity.java
@@ -1,15 +1,15 @@
 package com.wes.crash;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.kc.main.MainView;
 
 public class MainActivity extends Activity {
 	
 	protected void onCreate(Bundle savedInstanceState) {
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		super.onCreate(savedInstanceState);
 		setContentView(new MainView(this));
 	}
