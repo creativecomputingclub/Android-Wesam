84844f403bc12926101056a7a365c02814be5e6c
Wesam <whaddad16@yahoo.com>
2014-11-06 00:08:29 -0500

    ya, settings started to crash

--- Crash/src/com/wes/crash/Preferences.java
+++ Crash/src/com/wes/crash/Preferences.java
@@ -1,22 +1,55 @@
 package com.wes.crash;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceFragment{
+	
+	Intent i;
+	final static int cameraData = 0;
+	Preference button = (Preference)findPreference("camera");
+	
+
     @Override
-    protected void onCreate(final Bundle savedInstanceState){
+	public void onCreate(final Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+    	button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference arg0) { 
+                i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,cameraData);
+                return true;
+            }
+        });
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment{
+    @Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if(resultCode == Activity.RESULT_OK){
+			Bundle extras = data.getExtras();
+			Bitmap bmp = (Bitmap) extras.get("data");
+			Toast.makeText(getActivity(), "Image saved to:\n" +
+                    data.getData(), Toast.LENGTH_LONG).show();
+			
+		}
+	}
+
+
+	public static class MyPreferenceFragment extends PreferenceFragment{
         @Override
         public void onCreate(final Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.prefs);
         }
     }
+
+    
 
 }
--- Crash/src/com/wes/crash/Settings.java
+++
@@ -1,7 +0,0 @@
-package com.wes.crash;
-
-import android.preference.PreferenceActivity;
-
-public class Settings extends PreferenceActivity {
-
-}
